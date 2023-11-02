ff3d19df310c983b3fe2dcbbc847658dfe660ac0
s예찬 <davidsyc1170@gmail.com>
2023-11-03 07:49:40 +0900

    feat: init data 추가를 위한 applicationRunner 구현

---
+++ src/main/java/org/flow/assignment/configuration/InitDataSetting.java
@@ -0,0 +1,27 @@
+package org.flow.assignment.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.flow.assignment.entity.ExtensionFilter;
+import org.flow.assignment.repository.ExtensionFilterRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class InitDataSetting implements ApplicationRunner {
+    private final ExtensionFilterRepository filterRepository;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        List<String> fixedExtensions = new ArrayList<>(Arrays.asList("bat", "cmd", "com", "cpl", "exe", "scr", "js"));
+        List<ExtensionFilter> fixedExtFilters = new ArrayList<>();
+        fixedExtensions.forEach( ext -> fixedExtFilters.add(new ExtensionFilter(true, false, ext)));
+        filterRepository.saveAll(fixedExtFilters);
+    }
+
+}
